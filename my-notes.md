cf22ec02de75888779cba9ddf32bd5700d0812e7
ibakshay <akshay.iyyadurai.balasundaram@sap.com>
2022-05-14 08:04:00 +0200

    initial commit

---
+++ my-notes.md
@@ -0,0 +1,7 @@
+Application start up: 
+
+```
+1. npm run start:dev - to start the server
+2. docker-compose up - to start the database 
+3. npx prisma studio - Prisma client to view all the schemas 
+```
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,20 @@
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {
+  }
+
+  @Post('signup')
+  signup(@Body() dto: AuthDto) {
+    return this.authService.signUp(dto)
+  }
+
+  @Post()
+  login() {
+  }
+
+}
